96005c65f18772d3c1e3cb83c999703aaad0ccad
Edmund McKennall <ejmckennall@gmail.com>
2025-01-28 16:23:05 +1100

    more updates and trying to do particle density?

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 FC=gfortran
 FFLAGS=-O3 -Wall -Wextra # optimisation by 3 times
-SRC=output.f90 setup.f90 sph.f90
+SRC=output.f90 setup.f90 dens.f90 sph.f90
 OBJ=${SRC:.f90=.o}
 
 %.o: %.f90
 	$(FC) $(FFLAGS) -o $@ -c $<
+
+.PHONY: sph
 
 sph1: $(OBJ)
 	$(FC) $(FFLAGS) -o sph $(OBJ)
 
 clean:
 	rm -f .o .mod sph
+
+sph.o: setup.o setup.mod
--- setup.f90
+++ setup.f90
@@ -11,20 +11,24 @@ module setup
 
 contains
 
-subroutine do_setup(n,x)
+subroutine do_setup(n,x,v,m,h)
  integer, intent(out) :: n
- real, intent(out), allocatable :: x(:)
- real :: dx, xmin, xmax
+ real, intent(out), allocatable :: x(:),v(:),m(:),h(:)
+ real :: dx, xmin, xmax, rhozero
  integer :: i
 
  n = 100 
  xmin = 0.
  xmax = 1.
- allocate(x(n))
+ rhozero = 1.
+ allocate(x(n), v(n),m(n),h(n))
  dx = (xmax-xmin)/n
 
  do i = 1, n
   x(i) = xmin + (i-0.5)*dx
+  v(i) = 0.001*sin(2.*pi*x(i))
+  h(i) = 1.*dx
+  m(i) = rhozero*(xmax-xmin)/n
   enddo
 
 end subroutine do_setup
